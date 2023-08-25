320ab94bbbc0ffd75d6a2ab61bd209a61ad7734e
Omar Abdullah <omar.amaghawry@gmail.com>
2023-08-26 02:35:08 +0300

    edit config

--- src/main/java/com/efinance/repositories/user/UserRepository.java
+++ src/main/java/com/efinance/repositories/user/UserRepository.java
@@ -1,23 +1,27 @@
 package com.efinance.repositories.user;
 
+import java.util.List;
+
 import org.springframework.data.cassandra.repository.CassandraRepository;
+import org.springframework.data.cassandra.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.efinance.orm.user.User;
 
 public interface UserRepository extends CassandraRepository<User, String> {
 
-//	@Query("SELECT * FROM ZFIN_User u WHERE " + "(:P_IDS IS NULL OR u.id IN :P_IDS) "
-//			+ "AND (:P_EMAIL IS NULL OR u.contact.email = :P_EMAIL) "
-//			+ "AND (:P_EMAIL_VERIFIED IS NULL OR u.contact.emailVerified = :P_EMAIL_VERIFIED) "
-//			+ "AND (:P_PHONE_NUMBER IS NULL OR u.contact.phoneNumber = :P_PHONE_NUMBER) "
-//			+ "AND (:P_PHONE_NUMBER_VERIFIED IS NULL OR u.contact.phoneVerified = :P_PHONE_NUMBER_VERIFIED) "
-//			+ "AND (:P_TEXT IS NULL OR u.email LIKE %:P_TEXT% OR u.phoneNumber LIKE %:P_TEXT% ) "
-//			+ "AND (:P_BANNED IS NULL OR u.banned = :P_BANNED) " + "AND (:P_ACTIVE IS NULL OR u.active = :P_ACTIVE) "
-//			+ "AND (:P_ROLES IS NULL OR u.members.role IN :P_ROLES) "
-//			+ "AND (:P_ORGANIZATION_IDS IS NULL OR u.members.organization.id IN :P_ORGANIZATION_IDS)")
-//	List<User> findUsersByFilter(@Param("P_IDS") List<String> ids, @Param("P_EMAIL") String email,
-//			@Param("P_EMAIL_VERIFIED") Boolean emailVerified, @Param("P_PHONE_NUMBER") String phone,
-//			@Param("P_PHONE_NUMBER_VERIFIED") Boolean phoneVerified, @Param("P_TEXT") String text,
-//			@Param("P_BANNED") Boolean banned, @Param("P_ACTIVE") Boolean active, @Param("P_ROLES") List<String> roles,
-//			@Param("P_ORGANIZATION_IDS") List<String> organizationIds);
+	@Query("SELECT * FROM ZFIN_User u WHERE " + "(:P_IDS IS NULL OR u.id IN :P_IDS) "
+			+ "AND (:P_EMAIL IS NULL OR u.contact.email = :P_EMAIL) "
+			+ "AND (:P_EMAIL_VERIFIED IS NULL OR u.contact.emailVerified = :P_EMAIL_VERIFIED) "
+			+ "AND (:P_PHONE_NUMBER IS NULL OR u.contact.phoneNumber = :P_PHONE_NUMBER) "
+			+ "AND (:P_PHONE_NUMBER_VERIFIED IS NULL OR u.contact.phoneVerified = :P_PHONE_NUMBER_VERIFIED) "
+			+ "AND (:P_TEXT IS NULL OR u.email LIKE %:P_TEXT% OR u.phoneNumber LIKE %:P_TEXT% ) "
+			+ "AND (:P_BANNED IS NULL OR u.banned = :P_BANNED) " + "AND (:P_ACTIVE IS NULL OR u.active = :P_ACTIVE) "
+			+ "AND (:P_ROLES IS NULL OR u.members.role IN :P_ROLES) "
+			+ "AND (:P_ORGANIZATION_IDS IS NULL OR u.members.organization.id IN :P_ORGANIZATION_IDS)")
+	List<User> findUsersByFilter(@Param("P_IDS") List<String> ids, @Param("P_EMAIL") String email,
+			@Param("P_EMAIL_VERIFIED") Boolean emailVerified, @Param("P_PHONE_NUMBER") String phone,
+			@Param("P_PHONE_NUMBER_VERIFIED") Boolean phoneVerified, @Param("P_TEXT") String text,
+			@Param("P_BANNED") Boolean banned, @Param("P_ACTIVE") Boolean active, @Param("P_ROLES") List<String> roles,
+			@Param("P_ORGANIZATION_IDS") List<String> organizationIds);
 }
